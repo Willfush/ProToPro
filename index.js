d36b4b6d46438635705512d68e82686f1bbf0042
Willfush <willfush2@gmail.com>
2025-03-10 14:51:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// SCROLL ANIMATION 
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        // console.log(entry) // RISOLVERE PROBLEMA DI LAG TROPPE RICHIESTE
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el)=> observer.observe(el));
+
+
+
+
+
+
+const observer1 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements1 = document.querySelectorAll('.navBar-hidden');
+hiddenElements1.forEach((el)=> observer1.observe(el));
+
+// BUTTON ARROW TRANSISITION 
+
+const Preventivo = document.getElementById('Preventivo')
+
+Preventivo.addEventListener('mouseover', () =>{
+    Preventivo.classList.add('display-none')
+});
+
+Preventivo.addEventListener('mouseout', () =>{
+    Preventivo.classList.remove('display-none')
+});
